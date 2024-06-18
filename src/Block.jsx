13bc092a3e688d671312820c20435586b920b4ea
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-06-18 11:00:45 +0530

    Add grid

--- src/Block.jsx
+++ src/Block.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Block({shape, setShape}) {
   return (
-    <>
-        
-    </>
+    <div className='text-6xl hover:cursor-pointer'>
+      <p className='text-red-400 font-serif drop-shadow-2xl rounded-lg px-1'>X</p>  
+    </div>
   )
 }
 
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -8,32 +8,32 @@ function Grid() {
     <nav className='fixed top-0 w-screen text-center'>
       <h1 className='block mx-auto lg:w-80 text-red-50 font-serif font-extrabold text-2xl lg:text-4xl p-3 bg-red-400 drop-shadow-2xl mt-5 rounded-lg'>Tic-Tac-Toe</h1>
     </nav>
-    <div className='grid grid-cols-3 grid-rows-3 place-content-center place-items-center h-80 w-80 absolute inset-0 m-auto'>
-      <div className='bg-white h-14 w-14 p-2'>
+    <div className='grid grid-cols-3 grid-rows-3 place-content-stretch place-items-center h-96 w-96 absolute inset-0 m-auto border-4'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
-      <div className='bg-white h-14 w-14 p-2'>
+      <div className=''>
         <Block shape={shape} setShape={setShape} />
       </div>
     <p className='text-red-50 font-serif font-extrabold text-xl lg:text-4xl drop-shadow-2xl rounded-lg col-span-3 row-span-3'>
