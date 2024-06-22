69d323632729e123c80276f139879d0d1843a7b6
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-06-22 23:29:31 +0530

    Add X and O turns

--- src/Block.jsx
+++ src/Block.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function Block({shape, setShape}) {
+function Block({shape,onBlockClick}) {
   return (
-    <div className='text-6xl hover:cursor-pointer h-full flex items-center justify-center'>
+    <div 
+    onClick={onBlockClick}
+    className='text-6xl hover:cursor-pointer h-full flex items-center justify-center'>
       <p
-      onClick={() => shape === "X" ? setShape("O") : setShape("X")} 
-      className='text-red-400 font-serif drop-shadow-2xl rounded-lg px-1'>{shape}</p>  
+      className='text-red-400 font-serif drop-shadow-2xl rounded-lg px-1 high'>{shape}</p>  
     </div>
   )
 }
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -2,45 +2,39 @@ import React,{ useState } from 'react';
 import Block from './Block';
 
 function Grid() {
-  const [shape,setShape] = useState('X');
-  const [turn,setTurn] = useState();
+  const [turn,setTurn] = useState(true);
+  const [shapes,setShapes] = useState(Array(9).fill(null));
+
+  const handleClick = (i) =>{
+    const nextShapes = shapes.slice();
+    if(shapes[i] == null){
+      nextShapes[i] = turn ? 'X' : 'O';
+      setShapes(nextShapes);
+      setTurn(!turn);
+    }
+  }
+
   return (
     <>
     <nav className='fixed top-0 w-screen text-center'>
       <h1 className='block mx-auto lg:w-80 text-red-50 font-serif font-extrabold text-2xl lg:text-4xl p-3 bg-red-400 drop-shadow-2xl mt-5 rounded-lg'>Tic-Tac-Toe</h1>
     </nav>
     <div className='grid grid-cols-3 grid-rows-3 place-content-evenly place-items-stretch p-6 h-96 w-96 absolute inset-0 m-auto'>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
+      {
+      shapes.map((shape,index) =>{
+        return <div className='h-28 w-28 border-2 border-white'>
+        <Block 
+        key={index} 
+        shape={shape} 
+        onBlockClick={() => handleClick(index)
+      } />
       </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
-      <div className='h-28 w-28 border-2 border-white'>
-        <Block shape={shape} setShape={setShape} />
-      </div>
+      })}
     </div>
-    <p className='text-red-50 font-serif font-extrabold text-xl lg:text-4xl drop-shadow-2xl rounded-lg col-span-3 row-span-3 absolute bottom-44 left-[620px]'>
-      {shape=="X" ? "Player X's turn" : "Player O's turn"}
-    </p>
+
+    {<p className='text-red-50 font-serif font-extrabold text-xl lg:text-4xl drop-shadow-2xl rounded-lg col-span-3 row-span-3 absolute bottom-44 left-[620px]'>
+      {turn ? "Player X's turn" : "Player O's turn"}
+    </p>}
     </>
   )
 }
